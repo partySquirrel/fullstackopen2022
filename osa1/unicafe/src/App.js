8fa7354df67f236d10fa6b47b8bd7631d1c05b62
partySquirrel <putsepp@gmail.com>
2022-10-11 21:48:08 +0300

    refactor stats osa1.8

--- osa1/unicafe/src/App.js
+++ osa1/unicafe/src/App.js
@@ -3,22 +3,7 @@ import {useState} from 'react'
 const Header = ({text}) => <h1>{text}</h1>
 const Stats = ({text, value}) => <p>{text}: {value}</p>
 
-const Button = ({handleClick, text}) => (
-  <button onClick={handleClick}>
-    {text}
-  </button>
-)
-
-const App = () => {
-  // tallenna napit omaan tilaansa
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
-
-  const handleGood = () => setGood(good + 1)
-  const handleNeutral = () => setNeutral(neutral + 1)
-  const handleBad = () => setBad(bad + 1)
-
+const Statistics = ({good, neutral, bad}) => {
   // yhteenlasketun määrän
   const total = () => good + neutral + bad
 
@@ -37,18 +22,43 @@ const App = () => {
 
   return (
     <div>
+      <Header text='Statistics'/>
+
+      <Stats value={good} text='good'/>
+      <Stats value={neutral} text='neutral'/>
+      <Stats value={bad} text='bad'/>
+
+      <Stats value={total()} text='all'/>
+      <Stats value={average()} text='average'/>
+      <Stats value={positive()} text='positive'/>
+    </div>
+  )
+}
+
+const Button = ({handleClick, text}) => (
+  <button onClick={handleClick}>
+    {text}
+  </button>
+)
+
+const App = () => {
+  // tallenna napit omaan tilaansa
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const handleGood = () => setGood(good + 1)
+  const handleNeutral = () => setNeutral(neutral + 1)
+  const handleBad = () => setBad(bad + 1)
+
+  return (
+    <div>
       <Header text='Give Feedback'/>
       <Button handleClick={handleGood} text='good'/>
       <Button handleClick={handleNeutral} text='neutral'/>
       <Button handleClick={handleBad} text='bad'/>
 
-      <Header text='Statistics'/>
-      <Stats value={good} text='good'/>
-      <Stats value={neutral} text='neutral'/>
-      <Stats value={bad} text='bad'/>
-      <Stats value={total()} text='all'/>
-      <Stats value={average()} text='average'/>
-      <Stats value={positive()} text='positive'/>
+      <Statistics good={good} bad={bad} neutral={neutral}/>
     </div>
   )
 }
