f8d7060720accb306354048307acc409cd59af9d
partySquirrel <putsepp@gmail.com>
2022-10-17 18:06:38 +0300

    tehtävät 2.1 -2.3

--- osa3/kurssitiedot/src/App.js
+++ osa3/kurssitiedot/src/App.js
@@ -4,15 +4,12 @@ const Part = ({part}) => <p>{part.name} {part.exercises}</p>
 
 const Content = ({parts}) => (
   <div>
-    <Part part={parts[0]}/>
-    <Part part={parts[1]}/>
-    <Part part={parts[2]}/>
+    {parts.map(part => <Part key={part.id} part={part}/>)}
   </div>
 )
 
 const Total = ({parts}) => {
-  let sum = 0
-  parts.forEach(part => sum += part.exercises)
+  const sum = parts.reduce((previous, currentPart) => previous + currentPart.exercises, 0);
   return (
     <div>
       <p>Number of exercises {sum}</p>
@@ -20,30 +17,47 @@ const Total = ({parts}) => {
   )
 }
 
-const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-      {
-        name: 'State of a component',
-        exercises: 14
-      }
-    ]
-  }
-
+const Course = ({course}) => {
   return (
     <div>
       <Header name={course.name}/>
       <Content parts={course.parts}/>
       <Total parts={course.parts}/>
+    </div>
+  )
+}
+
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    id: 1,
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      },
+      {
+        name: 'Redux',
+        exercises: 11,
+        id: 4
+      },
+    ]
+  }
+
+  return (
+    <div>
+      <Course course={course}/>
     </div>
   )
 }
