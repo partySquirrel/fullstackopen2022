c14b6c6916c12040ee2fab5f9952df6ed1b4a6b7
partySquirrel <putsepp@gmail.com>
2022-10-19 21:29:06 +0300

    2.11 fetch from json db

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -1,18 +1,22 @@
-import {useState} from 'react'
+import {useEffect, useState} from 'react'
+import axios from "axios";
 import Filter from './components/Filter'
 import PersonForm from './components/PersonForm'
 import Persons from './components/Persons'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    {id: 1, name: 'Arto Hellas', number: '040-123456'},
-    {id: 2, name: 'Ada Lovelace', number: '39-44-5323523'},
-    {id: 3, name: 'Dan Abramov', number: '12-43-234345'},
-    {id: 4, name: 'Mary Poppendieck', number: '39-23-6423122'}
-  ])
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [newFilter, setNewFilter] = useState('')
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        setPersons(response.data)
+      })
+  }, [])
 
   const validate = () => {
     if (newName.trim().length < 1) {
