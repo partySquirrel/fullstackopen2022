0806f6531d24d38515a8654c06500eac370939cf
partySquirrel <putsepp@gmail.com>
2022-11-09 16:48:42 +0200

    4.4 total likes with test

--- osa4/tests/list_helper.test.js
+++ osa4/tests/list_helper.test.js
@@ -1,8 +1,19 @@
 const listHelper = require('../utils/list_helper')
+const { testBlogs } = require("./testData");
 
-test('dummy returns one', () => {
-  const blogs = []
+describe('total likes', () => {
+  test('when list is empty likes are 0', () => {
+    const result = listHelper.totalLikes([])
+    expect(result).toBe(0)
+  })
 
-  const result = listHelper.dummy(blogs)
-  expect(result).toBe(1)
+  test('when list has only one blog equals the likes of that', () => {
+    const result = listHelper.totalLikes(testBlogs.slice(0,1))
+    expect(result).toBe(7)
+  })
+
+  test('when list has blogs blog equals the sum of likes', () => {
+    const result = listHelper.totalLikes(testBlogs)
+    expect(result).toBe(36)
+  })
 })
