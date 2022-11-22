dc433dbc3ec42b76d1fbf92bbf33f56fc57984d4
partySquirrel <putsepp@gmail.com>
2022-11-22 16:49:10 +0200

    osa 5.7 toggle blog full info

--- osa5/bloglist-frontend/src/components/Blog.js
+++ osa5/bloglist-frontend/src/components/Blog.js
@@ -1,7 +1,41 @@
-const Blog = ({blog}) => (
-  <div>
-    <strong>{blog.title}</strong> by {blog.author}
-  </div>  
-)
+import React, { useState } from "react";
+
+const Blog = ({ blog }) => {
+  const blogStyle = {
+    padding: 10,
+    border: 'solid',
+    borderWidth: 1,
+    borderColor: '#ccc',
+    marginBottom: 5,
+  }
+  const buttonStyle = {
+    marginLeft: 5
+  }
+
+  const [showAll, setShowAll] = useState(false)
+
+  const getDetails = () => {
+    return (
+      <>
+        <p><a href="{blog.url}">{blog.url}</a></p>
+        <p>Likes: {blog.likes}</p>
+        {
+          blog.user && <p>{blog.user.name}</p>
+        }
+      </>
+    )
+  }
+  return (
+    <div style={blogStyle}>
+      <p>
+        <strong>{blog.title}</strong> by {blog.author}
+        {showAll && <button style={buttonStyle} onClick={() => setShowAll(false)}>Hide</button>}
+        {!showAll && <button style={buttonStyle} onClick={() => setShowAll(true)}>View</button>}
+      </p>
+
+      {showAll && getDetails()}
+    </div>
+  )
+}
 
 export default Blog
