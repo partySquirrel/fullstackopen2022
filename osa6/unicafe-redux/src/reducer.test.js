5ee72fa7a7b28dd919f9fd100526999d04773469
partySquirrel <putsepp@gmail.com>
2022-11-30 13:01:17 +0200

    osa 6.1 add tests

--- osa6/unicafe-redux/src/reducer.test.js
+++ osa6/unicafe-redux/src/reducer.test.js
@@ -13,7 +13,7 @@ describe('unicafe reducer', () => {
     const action = {
       type: 'DO_NOTHING'
     }
-
+    deepFreeze(state)
     const newState = counterReducer(undefined, action)
     expect(newState).toEqual(initialState)
   })
@@ -30,6 +30,36 @@ describe('unicafe reducer', () => {
       good: 1,
       ok: 0,
       bad: 0
+    })
+  })
+
+  test('ok is incremented', () => {
+    const action = {
+      type: 'OK'
+    }
+    const state = initialState
+
+    deepFreeze(state)
+    const newState = counterReducer(state, action)
+    expect(newState).toEqual({
+      good: 0,
+      ok: 1,
+      bad: 0
+    })
+  })
+
+  test('bad is incremented', () => {
+    const action = {
+      type: 'BAD'
+    }
+    const state = initialState
+
+    deepFreeze(state)
+    const newState = counterReducer(state, action)
+    expect(newState).toEqual({
+      good: 0,
+      ok: 0,
+      bad: 1
     })
   })
 })
