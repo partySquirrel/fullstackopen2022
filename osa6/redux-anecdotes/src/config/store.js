88b20a324576368d9eedf945c6287bf15df222d3
partySquirrel <putsepp@gmail.com>
2022-12-01 14:20:12 +0200

    osa 6.9 use Redux Toolkit configureStore

---
+++ osa6/redux-anecdotes/src/config/store.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { configureStore } from "@reduxjs/toolkit";
+import anecdoteReducer from "../reducers/anecdoteReducer";
+
+const store = configureStore({
+  reducer: {
+    anecdotes: anecdoteReducer
+  }
+})
+
+console.log(store.getState())
+
+export default store
