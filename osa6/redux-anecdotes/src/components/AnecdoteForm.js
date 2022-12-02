ffb329060fc96f98b432a8a268fab018b4b282b0
partySquirrel <putsepp@gmail.com>
2022-12-02 13:23:02 +0200

    osa 6.15-6.16 add Redux Thunk

--- osa6/redux-anecdotes/src/components/AnecdoteForm.js
+++ osa6/redux-anecdotes/src/components/AnecdoteForm.js
@@ -1,7 +1,6 @@
 import { useDispatch } from 'react-redux'
 import { createAnecdote } from '../reducers/anecdoteReducer'
 import { showNotification } from '../reducers/notificationReducer'
-import anecdoteService from '../services/anecdotes'
 
 const AnecdoteForm = () => {
   const dispatch = useDispatch()
@@ -10,8 +9,7 @@ const AnecdoteForm = () => {
     event.preventDefault()
     const content = event.target.anecdote.value
     event.target.anecdote.value = ''
-    const created = await anecdoteService.createNew(content)
-    dispatch(createAnecdote(created))
+    dispatch(createAnecdote(content))
     dispatch(showNotification(`Created '${content}'`))
   }
 
--- osa6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ osa6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -1,12 +1,10 @@
 import { createSlice } from '@reduxjs/toolkit'
+import anecdoteService from '../services/anecdotes'
 
 const anecdoteSlice = createSlice({
   name: 'anecdotes',
   initialState: [],
   reducers: {
-    createAnecdote(state, action) {
-      state.push(action.payload)
-    },
     voteForAnecdote(state, action) {
       const id = action.payload
       const itemToChange = state.find(item => item.id === id)
@@ -27,5 +25,20 @@ const anecdoteSlice = createSlice({
   },
 })
 
-export const { createAnecdote, voteForAnecdote, appendAnecdote, setAnecdotes } = anecdoteSlice.actions
+export const {  voteForAnecdote, appendAnecdote, setAnecdotes } = anecdoteSlice.actions
+
+export const initializeAnecdotes = () => {
+  return async dispatch => {
+    const items = await anecdoteService.getAll()
+    dispatch(setAnecdotes(items))
+  }
+}
+
+export const createAnecdote = content => {
+  return async dispatch => {
+    const item = await anecdoteService.createNew(content)
+    dispatch(appendAnecdote(item))
+  }
+}
+
 export default anecdoteSlice.reducer
