5d80d253deb4d9ec3a99b6de6e622e126833f264
partySquirrel <putsepp@gmail.com>
2022-12-08 18:34:08 +0200

    7.8: ultimate hooks

--- osa7/ultimate-hooks/src/App.js
+++ osa7/ultimate-hooks/src/App.js
@@ -18,10 +18,21 @@ const useField = (type) => {
 const useResource = (baseUrl) => {
   const [resources, setResources] = useState([])
 
-  // ...
+  useEffect(() => {
+    axios.get(baseUrl)
+      .then(response => {
+        setResources( response.data)
+      })
+      .catch(error => {
+        console.log(error)
+      })
+  }, [baseUrl])
 
   const create = (resource) => {
-    // ...
+    axios.post(baseUrl, resource)
+      .then(response => {
+        setResources([...resources, response.data])
+      })
   }
 
   const service = {
