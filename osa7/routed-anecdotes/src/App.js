b5936d89e494153ae894062cfc5c7628de4858b9
partySquirrel <putsepp@gmail.com>
2022-12-08 17:56:44 +0200

    osa 7.6 rename reset to onReset() to avoid input error with spread

--- osa7/routed-anecdotes/src/App.js
+++ osa7/routed-anecdotes/src/App.js
@@ -83,9 +83,9 @@ const CreateNew = (props) => {
   }
   const handleReset = (e) => {
     e.preventDefault()
-    content.reset()
-    author.reset()
-    info.reset()
+    content.onReset()
+    author.onReset()
+    info.onReset()
   }
 
   return (
