528a60138a67f57c445c622718730e5ff79d8e24
partySquirrel <putsepp@gmail.com>
2022-12-08 17:36:22 +0200

    format

--- osa7/routed-anecdotes/src/App.js
+++ osa7/routed-anecdotes/src/App.js
@@ -21,7 +21,7 @@ const AnecdoteList = ({ anecdotes }) => (
   <div>
     <h2>Anecdotes</h2>
     <ul>
-      {anecdotes.map(anecdote =><li key={anecdote.id}><AnecdoteListItem anecdote={anecdote} /></li>)}
+      {anecdotes.map(anecdote => <li key={anecdote.id}><AnecdoteListItem anecdote={anecdote}/></li>)}
     </ul>
   </div>
 )
@@ -47,8 +47,10 @@ const About = () => (
     <p>According to Wikipedia:</p>
 
     <em>An anecdote is a brief, revealing account of an individual person or an incident.
-      Occasionally humorous, anecdotes differ from jokes because their primary purpose is not simply to provoke laughter but to reveal a truth more general than the brief tale itself,
-      such as to characterize a person by delineating a specific quirk or trait, to communicate an abstract idea about a person, place, or thing through the concrete details of a short narrative.
+      Occasionally humorous, anecdotes differ from jokes because their primary purpose is not simply to provoke laughter
+      but to reveal a truth more general than the brief tale itself,
+      such as to characterize a person by delineating a specific quirk or trait, to communicate an abstract idea about a
+      person, place, or thing through the concrete details of a short narrative.
       An anecdote is "a story with a point."</em>
 
     <p>Software engineering is full of excellent anecdotes, at this app you can find the best and add more.</p>
@@ -59,7 +61,9 @@ const Footer = () => (
   <div>
     Anecdote app for <a href='https://fullstackopen.com/'>Full Stack Open</a>.
 
-    See <a href='https://github.com/fullstack-hy2020/routed-anecdotes/blob/master/src/App.js'>https://github.com/fullstack-hy2020/routed-anecdotes/blob/master/src/App.js</a> for the source code.
+    See <a
+    href='https://github.com/fullstack-hy2020/routed-anecdotes/blob/master/src/App.js'>https://github.com/fullstack-hy2020/routed-anecdotes/blob/master/src/App.js</a> for
+    the source code.
   </div>
 )
 
--- osa7/routed-anecdotes/src/index.js
+++ osa7/routed-anecdotes/src/index.js
@@ -5,6 +5,6 @@ import { BrowserRouter as Router } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Router>
-    <App />
+    <App/>
   </Router>
 )
